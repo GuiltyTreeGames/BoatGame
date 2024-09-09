3587c41f539152ffcb170af69c0f1b8e4d88e980
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-08 22:51:47 -0400

    code: add fade in/out
    Add black screen that fades in and out when changing rooms

--- Assets/Code/UI/FadeWidget.cs
+++ Assets/Code/UI/FadeWidget.cs
@@ -1,7 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FadeWidget : MonoBehaviour
 {
+    private Image image;
+
+    private void OnEnable()
+    {
+        Core.RoomManager.OnFade += OnFade;
+    }
+
+    private void OnDisable()
+    {
+        Core.RoomManager.OnFade -= OnFade;
+    }
+
+    void Start()
+    {
+        image = GetComponent<Image>();
+        image.color = new Color32(0, 0, 0, 0);
+    }
+
+    void OnFade(float percent)
+    {
+        image.color = new Color32(0, 0, 0, (byte)(percent * 255));
+    }
 }
