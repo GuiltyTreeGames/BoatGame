b71c0d6a8e529acfe09633d7e7d207dc518aa42a
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-11 19:39:42 -0400

    code: block gameplay input on main menu
    Modify MenuLogic to also block input

--- Assets/Code/Components/OpenMainMenu.cs
+++ Assets/Code/Components/OpenMainMenu.cs
@@ -2,8 +2,26 @@ using UnityEngine;
 
 public class OpenMainMenu : MonoBehaviour
 {
+    private void OnEnable()
+    {
+        Core.InputManager.AddInputBlock(MENU_BLOCK);
+    }
+
+    private void OnDisable()
+    {
+        Core.InputManager.RemoveInputBlock(MENU_BLOCK);
+    }
+
     void Start()
     {
         Core.CanvasManager.MainMenuWidget.SetMenuStatus(true);
     }
+
+    private static readonly InputBlock MENU_BLOCK = new InputBlock(new InputType[]
+    {
+        InputType.MoveHorizontal,
+        InputType.MoveVertical,
+        InputType.Interact,
+        InputType.Pause
+    });
 }
