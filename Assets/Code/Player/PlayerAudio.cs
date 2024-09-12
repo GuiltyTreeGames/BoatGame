9e4fdda4b6f7bc9abb455d22014c40e825932a10
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-11 20:51:09 -0400

    code: test what floor object player is on

--- Assets/Code/Player/PlayerAudio.cs
+++ Assets/Code/Player/PlayerAudio.cs
@@ -1,15 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerAudio : MonoBehaviour
 {
     private AudioSource source;
+    private readonly Collider2D[] _floorColliders = new Collider2D[1];
 
     [SerializeField]
     private AudioClip[] _footstepSounds;
     [SerializeField]
     private float _secondsBetweenPlay;
+    [SerializeField]
+    private LayerMask _floorLayer;
 
     private int _currentIndex = 0;
     private float _currentPlayTimer = 0;
@@ -30,6 +31,12 @@ public class PlayerAudio : MonoBehaviour
         {
             _currentPlayTimer = _secondsBetweenPlay / 2;
             return;
+        }
+
+        // Check the current floor type
+        if (Physics2D.OverlapCircleNonAlloc(transform.position, 0.2f, _floorColliders, _floorLayer) > 0)
+        {
+            Debug.Log("Walking on " + _floorColliders[0].gameObject.name);
         }
 
         // Increase timer
