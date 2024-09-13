c46fe2472340d2ff744c0517c692f7968b3606b5
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-12 21:55:54 -0400

    code: allow dragging planks around

--- Assets/Code/Tasks/ShipRepair/Plank.cs
+++ Assets/Code/Tasks/ShipRepair/Plank.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class Plank : MonoBehaviour
+public class Plank : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
 {
     private RectTransform rect;
 
     [SerializeField]
     private float _dropSpeed = 1;
+
+    private bool _isDragging = false;
+    private Vector2 _dragOffset;
 
     void Start()
     {
@@ -16,6 +20,9 @@ public class Plank : MonoBehaviour
 
     void Update()
     {
+        if (_isDragging)
+            return;
+
         // If floating, apply gravity
         if (rect.anchoredPosition.y > MIN_POSITION)
         {
@@ -27,6 +34,27 @@ public class Plank : MonoBehaviour
         {
             rect.anchoredPosition = new Vector2(rect.anchoredPosition.x, MIN_POSITION);
         }
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log(eventData.position);
+
+        float x = Mathf.Clamp(eventData.position.x, MIN_POSITION, Screen.width - MIN_POSITION);
+        float y = Mathf.Clamp(eventData.position.y, MIN_POSITION, Screen.height - MIN_POSITION);
+
+        transform.position = _dragOffset + new Vector2(x, y);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        _isDragging = true;
+        _dragOffset = (Vector2)transform.position - eventData.position;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        _isDragging = false;
     }
 
     private const float MIN_POSITION = 10f;
