e3d280aeccd2e319d2d4e5a1591a55ca3b9e15ba
Branden Wood <44546325+BrandenEK@users.noreply.github.com>
2024-09-18 16:01:46 -0400

    code: add change room method
    Add ChangeRoom(room) that unloads then loads the next room

--- Assets/Code/Framework/NewRoomManager.cs
+++ Assets/Code/Framework/NewRoomManager.cs
@@ -5,17 +5,48 @@ using UnityEngine.SceneManagement;
 
 public class NewRoomManager : BaseManager
 {
+    public string CurrentRoom { get; private set; } = string.Empty;
+
     public void LoadMainMenu()
     {
         Debug.Log($"Loading main menu");
+        CurrentRoom = string.Empty;
+
         SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
     }
 
     public void LoadGameplay(string room)
     {
         Debug.Log($"Loading gameplay ({room})");
+        CurrentRoom = room;
+
         SceneManager.LoadScene("Gameplay", LoadSceneMode.Single);
-        SceneManager.LoadSceneAsync($"Design/Scenes/Ship/{room}/LAYOUT", LoadSceneMode.Additive);
-        SceneManager.LoadSceneAsync($"Design/Scenes/Ship/{room}/LOGIC", LoadSceneMode.Additive);
+        LoadAllScenes(room);
     }
+
+    public void ChangeRoom(string room)
+    {
+        if (SceneManager.GetActiveScene().name != "Gameplay")
+            throw new System.Exception("Tried to change room while not in gameplay");
+
+        Debug.Log($"Changing room to {room}");
+        UnloadAllScenes(CurrentRoom);
+        CurrentRoom = room;
+        LoadAllScenes(CurrentRoom);
+    }
+
+    private void LoadAllScenes(string room)
+    {
+        SceneManager.LoadSceneAsync(GetLayoutPath(room), LoadSceneMode.Additive);
+        SceneManager.LoadSceneAsync(GetLogicPath(room), LoadSceneMode.Additive);
+    }
+
+    private void UnloadAllScenes(string room)
+    {
+        SceneManager.UnloadSceneAsync(GetLayoutPath(CurrentRoom));
+        SceneManager.UnloadSceneAsync(GetLogicPath(CurrentRoom));
+    }
+
+    private string GetLayoutPath(string room) => $"Design/Scenes/Ship/{room}/LAYOUT";
+    private string GetLogicPath(string room) => $"Design/Scenes/Ship/{room}/LOGIC";
 }
